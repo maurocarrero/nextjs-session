440e7e18e91d4e854d74ea3fb5e9d3f769ce1730
Mauro Carrero <mauro.carrero@sabre.com>
2020-01-16 21:49:51 -0300

    feat(step2): ssr
    add style to SSR page

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../src/styles.css';
+
+export default function SessionApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/ssr.js
+++ pages/ssr.js
@@ -1,9 +1,30 @@
 const SSRPage = ({ data }) => (
   <section>
-    <h1>Pre-render: SSR</h1>
+    <h1 className="morado">Pre-render: SSR</h1>
     <p>This page is rendered on demand providing last updated data.</p>
     <p>{data}</p>
+    <p className="morado">
+      {'Styled using '}
+      <a target="_blank" href={'https://github.com/zeit/styled-jsx'}>
+        styled-jsx
+      </a>
+      {'.'}
+    </p>
     <a href="/">Home</a>
+    <style jsx>{`
+      .morado {
+        color: violet;
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background: #010101;
+        color: #eeeeee;
+      }
+      .morado {
+        color: violet;
+      }
+    `}</style>
   </section>
 );
 
