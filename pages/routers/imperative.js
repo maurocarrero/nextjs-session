6ff71f4e6ec660c2dcca8869b9fff57156e61637
Mauro Carrero <mauro.carrero@sabre.com>
2020-02-18 11:43:03 -0300

    add shallow routing example

--- pages/routers/imperative.js
+++ pages/routers/imperative.js
@@ -1,10 +1,28 @@
 import Router from 'next/router';
+import { withRouter } from 'next/router';
 
-const DoSomethingRouter = () => (
+const DoSomethingRouter = ({ router }) => (
   <section>
     <h2>Imperative routing</h2>
     <button onClick={() => Router.push('/')}>Go home</button>
+    <button
+      onClick={() =>
+        Router.push(
+          '/routers/imperative?counter=10',
+          { auth: null },
+          { shallow: true }
+        )
+      }
+    >
+      Refresh this page (shallow routing)
+    </button>
+    <div>Query: {JSON.stringify(router.query)}</div>
   </section>
 );
 
-export default DoSomethingRouter;
+DoSomethingRouter.getInitialProps = async () => {
+  console.log('/routers/imperative :: shallow routing');
+  return {};
+};
+
+export default withRouter(DoSomethingRouter);
